1ba23bf6e73192943020826c792d07c0852bf1cc
LazySquirrel <dmi3evatv@gmail.com>
2024-03-24 22:40:37 +0300

    update content

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            Happy Travel Agency
+            Happy Travel Tours
           </Link>
         </Typography>
 
