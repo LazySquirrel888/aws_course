68533c5241b83dd29ecbf982904f81dfb68ec3eb
LazySquirrel <dmi3evatv@gmail.com>
2024-03-24 19:45:11 +0300

    change theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,22 @@
 import { createTheme } from "@mui/material/styles";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0097A7",
+    },
+    secondary: {
+      main: "#CDDC39",
+    },
+    error: {
+      main: "#f44336",
+    },
+    background: {
+      default: "#fff",
+    },
+  },
+  typography: {
+    fontFamily: "Arial",
+  },
+});
+
